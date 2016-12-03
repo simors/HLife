06515d3f9dbe24017978db1170986c389b72c588
reluo <21404683@qq.com>
2016-12-03 17:06:15 +0800

    RegistByLiLu

--- app/scenes/scenes.js
+++ app/scenes/scenes.js
@@ -14,10 +14,10 @@ export const scenes = Actions.create(
   <Scene key="root" hideNavBar={true}>
     <Scene key="LAUNCH" component={Launch} />
     <Scene key="HOME" component={Home} />
-    <Scene key="LOGIN" component={Login} />
+    <Scene key="LOGIN" component={Login} initial={true} />
     <Scene key="REGIST" component={Regist} />
     <Scene key="REG4VERIFYCODE" component={RegVerifyCode} />
-    <Scene key="RetrivevePassword" component={RetrivevePassword}/>
-    <Scene key="SETPSW" component={SetPsw} initial={true}/>
+    <Scene key="RetrivevePassword" component={RetrivevePassword} />
+    <Scene key="SETPSW" component={SetPsw} />
   </Scene>
 )
