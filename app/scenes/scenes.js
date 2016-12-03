0facdd2f81c66dba7dc4fb170465cd40695e6e26
李鲁 <lilu@liludeMacBook-Pro.local>
2016-12-03 10:20:27 +0800

    2016122

--- app/scenes/scenes.js
+++ app/scenes/scenes.js
@@ -7,13 +7,14 @@ import Home from '../components/Home'
 import Login from '../components/Login'
 import Regist from '../components/Login/Regist'
 import Reg4VerifyCode from '../components/Login/Reg4VerifyCode'
-
+import SetPsw from '../components/Login/SetPsw/index'
 export const scenes = Actions.create(
   <Scene key="root" hideNavBar={true}>
-    <Scene key="LAUNCH" component={Launch} initial={true} />
+    <Scene key="LAUNCH" component={Launch}  />
     <Scene key="HOME" component={Home} />
     <Scene key="LOGIN" component={Login} />
     <Scene key="REGIST" component={Regist} />
-    <Scene key="REG4VERIFYCODE" component={Reg4VerifyCode} />
+      <Scene key="REG4VERIFYCODE" component={Reg4VerifyCode} />
+      <Scene key="SETPSW" component={SetPsw} initial={true}/>
   </Scene>
 )
