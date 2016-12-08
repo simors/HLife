406d08e9322bf421cac259630ca474cd148921a8
zachary <443428762@qq.com>
2016-12-08 14:18:41 +0800

    Login index style modify

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -122,42 +122,6 @@ const styles = StyleSheet.create({
     marginRight: normalizeW(17),
     marginBottom: normalizeW(25)
   },
-  header: {
-    backgroundColor: '#f3f3f3',
-    paddingTop: normalizeH(20),
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    borderBottomWidth: 1,
-    borderBottomColor: '#B2B2B2'
-  },
-  chevronLeft: {
-    position: 'absolute',
-    left: 9,
-    bottom: 14,
-    width: 13,
-    height: 21,
-    zIndex: 10,
-    fontSize: em(24),
-    color: '#50E3C2',
-  },
-  title: {
-    flex: 1,
-    lineHeight: 44,
-    fontSize: em(17),
-    color: '#030303',
-    textAlign: 'center'
-  },
-  rightBox: {
-    position: 'absolute',
-    right: 9,
-    bottom: 14,
-    zIndex: 10,
-  },
-  rightTxt: {
-    fontSize: em(17),
-    color: '#50E3C2',
-    textAlign: 'right',
-  },
   body: {
     paddingTop: normalizeH(65),
     width: PAGE_WIDTH,
@@ -167,19 +131,6 @@ const styles = StyleSheet.create({
     marginBottom: normalizeH(44),
     width: 108,
     height: normalizeH(47),
-  },
-  inputField: {
-    height: normalizeH(50),
-    marginLeft: normalizeW(17),
-    marginRight: normalizeW(17),
-    marginBottom: normalizeH(31),
-    paddingLeft: normalizeW(10),
-    paddingRight: normalizeW(10),
-    borderWidth: 1,
-    borderColor: '#E9E9E9',
-    backgroundColor: '#F3F3F3',
-    color: '#666',
-    fontSize: em(14)
   },
   btn: {
     height: normalizeH(50),
