bc98e40c8cd4cb53ab2bcfddab92cb543b8fc90d
wuleon <wu.xyljp@foxmail.com>
2016-12-08 20:54:30 +0800

    modify sms auth and regist

--- app/components/Login/Regist.js
+++ app/components/Login/Regist.js
@@ -9,7 +9,6 @@ import {
   Alert,
   Dimensions
 } from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {Actions} from 'react-native-router-flux'
@@ -21,10 +20,11 @@ import {
 
 import SmsAuthCodeInput from '../common/Input/SmsAuthCodeInput'
 import PhoneInput from '../common/Input/PhoneInput'
+import PasswordInput from '../common/Input/PasswordInput'
+
 import Header from '../common/Header'
 
 const PAGE_WIDTH=Dimensions.get('window').width
-const PAGE_HEIGHT=Dimensions.get('window').height
 
 let commonForm = Symbol('commonForm')
 const phoneInput = {
@@ -35,6 +35,11 @@ const phoneInput = {
 const smsAuthCodeInput = {
   formKey: commonForm,
   stateKey: Symbol('smsAuthCodeInput')
+}
+
+const passwordInput = {
+  formKey: commonForm,
+  stateKey: Symbol('passwordInput')
 }
 
 class Regist extends Component {
@@ -69,34 +74,31 @@ class Regist extends Component {
           rightPress={() => Actions.LOGIN()}
         />
         <View style={styles.body}>
-          <Image source={require('../../assets/images/login_weixin@1x.png')} style={styles.logo}></Image>
-          <PhoneInput {...phoneInput}/>
-          <SmsAuthCodeInput {...smsAuthCodeInput}
-                            style={{marginLeft:normalizeW(17),
-                                    marginRight:normalizeW(17),
-                                    marginBottom:normalizeH(25)}}/>
-          <View style={styles.inputField}>
-          <TextInput
-            placeholder={'设置密码（6～10位数字或字母'}
-            style={styles.pswText}
-            autoCapitalize={'none'}
-            autoCorrect={false}
-            onChangeText={(text) => {
-              this.changeUserState('userName', text)
-            }}
-          />
-            <Image source={require('../../assets/images/code_close_eye.png')} style={styles.pswEye}></Image>
+          <Image source={require('../../assets/images/login_weixin@1x.png')} style={styles.logo} />
 
-          </View>
+          <PhoneInput {...phoneInput}
+                      containerStyle={{marginLeft:normalizeH(17),
+                        marginRight:normalizeH(17),
+                        marginBottom:normalizeH(25)}}/>
+
+          <SmsAuthCodeInput {...smsAuthCodeInput}
+                            containerStyle={{marginLeft:normalizeH(17),
+                              marginRight:normalizeH(17),
+                              marginBottom:normalizeH(25)}}/>
+
+          <PasswordInput {...passwordInput}
+                            containerStyle={{marginLeft:normalizeH(17),
+                              marginRight:normalizeH(17),
+                              marginBottom:normalizeH(25)}}/>
+
           <Button
             buttonStyle={styles.btn}
             onPress={this.onButtonPress}
             title="开始使用"
           />
-          <View style={styles.agreementview}>
-            <Image source={require('../../assets/images/code_close_eye.png')} style={styles.check}></Image>
-            <Text style={styles.agreementtext} onPress={this.retrievePassword}>服务条款及协议</Text>
-
+          <View style={styles.agreementView}>
+            <Image source={require('../../assets/images/code_close_eye.png')} style={styles.check} />
+            <Text style={styles.agreementText} onPress={this.retrievePassword}>服务条款及协议</Text>
           </View>
           <SnsLogin />
         </View>
@@ -125,97 +127,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  header: {
-    backgroundColor: '#f3f3f3',
-    paddingTop: normalizeH(20),
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    borderBottomWidth: 1,
-    borderBottomColor: '#B2B2B2'
-  },
-  chevronLeft: {
-    position: 'absolute',
-    left: 9,
-    bottom: 14,
-    width: 13,
-    height: 21,
-    zIndex: 10,
-    fontSize: em(24),
-    color: '#50E3C2',
-  },
-  title: {
-    flex: 1,
-    lineHeight: 44,
-    fontSize: em(16),
-    color: '#030303',
-    textAlign: 'center'
-  },
-  rightBox: {
-    position: 'absolute',
-    right: 9,
-    bottom: 14,
-    zIndex: 10,
-  },
-  rightTxt: {
-    fontSize: em(17),
-    color: '#50E3C2',
-    textAlign: 'right',
-  },
   body: {
-    //paddingTop: normalizeH(65),
     width: PAGE_WIDTH,
   },
   logo: {
     marginLeft: normalizeW(PAGE_WIDTH / 2 - 54),
     marginBottom: normalizeH(25),
     marginTop:normalizeH(25),
-    width: 108,
+    width: normalizeH(108),
     height: normalizeH(47),
-  },
-  inputField: {
-    height: normalizeH(50),
-    marginLeft: normalizeW(17),
-    marginRight: normalizeW(17),
-    marginBottom: normalizeH(25),
-    paddingLeft: normalizeW(10),
-    paddingRight: normalizeW(10),
-    borderWidth: 1,
-    borderColor: '#E9E9E9',
-    backgroundColor: '#F3F3F3',
-    color: '#666',
-    fontSize: em(14),
-    flexDirection:'row',
-  },
-  pswText: {
-
-    marginLeft: normalizeW(3),
-    padding:normalizeH(5),
-    fontSize:em(14),
-    width:normalizeW(233),
-
-    backgroundColor: '#F3F3F3',
-    color: '#666',
-    fontSize: em(14),
-    //flex:5,
-  },
-  inputverifycode:{
-    height:normalizeH(50),
-    width:normalizeW(196),
-  },
-  pswEye: {
-    left: normalizeW(27),
-    top: normalizeH(19),
-    width: normalizeW(35),
-    height: normalizeH(12),
-    //color:'#50E3C2'
-    //flex:1,
-  },
-  getverifycodebtn:{
-    marginLeft:0,
-    marginTop:0,
-    height:normalizeH(50),
-    width:normalizeW(134),
-    backgroundColor:'#50E3C2',
   },
   btn: {
     height: normalizeH(50),
@@ -224,7 +144,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#50E3C2',
     marginBottom: normalizeH(24)
   },
-  agreementview:{
+  agreementView:{
     height:normalizeH(17),
     flexDirection:'row',
     justifyContent:'center',
@@ -234,9 +154,8 @@ const styles = StyleSheet.create({
     width:normalizeW(18),
     height:normalizeH(16),
   },
-  agreementtext:{
+  agreementText:{
     fontSize:em(14),
     color:'#50E3C2',
-
   },
 })
