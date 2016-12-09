dc883d16851ee39f67625e48e79380be7c459728
yang24201 <yang24201@126.com>
2016-12-09 21:00:00 +0800

    add RichTextInput in ios

---
+++ app/components/common/Input/RichTextInput.js
@@ -0,0 +1,335 @@
+/**
+ * Created by yangyang on 2016/12/8.
+ */
+import React, {Component} from 'react'
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Dimensions,
+  Platform,
+} from 'react-native'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+import RNFS from 'react-native-fs'
+import WebViewBridge from 'react-native-webview-bridge'
+
+var toolDefault = [
+  require('../../../assets/images/indent.png'),
+  require('../../../assets/images/bold.png'),
+  require('../../../assets/images/blockquote.png'),
+  require('../../../assets/images/publish_tool_image.png'),
+]
+var toolSelect = [
+  require('../../../assets/images/indent.png'),
+  require('../../../assets/images/bold_sel.png'),
+  require('../../../assets/images/blockquote_sel.png'),
+  require('../../../assets/images/publish_tool_image.png'),
+]
+
+var tools = [
+  {type: 'indent', icon: toolDefault[0]},
+  {type: 'bold', icon: toolDefault[1]},
+  {type: 'blockquote', icon: toolDefault[2]},
+  {type: 'image', icon: toolDefault[3]},
+]
+
+const GET_FOCUS = 'GET_FOCUS'
+const LOSE_FOCUS = 'LOSE_FOCUS'
+const LOAD_DRAFT = 'LOAD_DRAFT'
+const COUNTER = 'COUNTER'
+const CONTENTS = 'CONTENTS'
+const HEIGHT = 'HEIGHT'
+
+const PAGE_WIDTH=Dimensions.get('window').width
+const PAGE_HEIGHT = Dimensions.get('window').height
+const MIN_RTE_HEIGHT = 400
+// const navBarPadding = (Platform.OS == 'android' ? 50 : 64)
+const navBarPadding = 0
+
+class RichTextInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      closeTips: {height: 0},
+      toolSelect: tools.map((tool, index) => {
+        return {select: false, index: index}
+      }),
+      webViewHeight: MIN_RTE_HEIGHT,
+    }
+  }
+
+  renderWebView() {
+    console.log(RNFS.MainBundlePath + "/richTextEdit.html")
+    const source = Platform.OS == 'ios' ?
+    {uri: RNFS.MainBundlePath + "/richTextEdit.html"} : {uri: "file:///android_asset/richTextEdit.html"}
+
+    return (
+      <WebViewBridge
+        ref={(web) => {
+          this.webView = web
+        }}
+        onBridgeMessage={this.onBridgeMessage.bind(this)}
+        injectedJavaScript={injectedJavaScript}
+        hideKeyboardAccessoryView={true}
+        automaticallyAdjustContentInsets={true}
+        source={source}
+        style={{width: Dimensions.get('window').width, flex: 1, backgroundColor: 'white'}}
+      />
+    )
+  }
+
+  renderHideEditToolView = () => {
+    return ([
+      <View style={{width: 1, backgroundColor: '#eeeeee'}}/>,
+      <TouchableOpacity style={styles.editToolKeyboardHide} onPress={() => {
+        this.webView.sendToBridge('keyboard_hide')
+      }}>
+        <Image source={require('../../../assets/images/keyboad_down.png')}/>
+      </TouchableOpacity>
+    ])
+  }
+
+  renderEditToolView() {
+    return (
+      <View>
+        <View style={{height: 1, backgroundColor: '#eeeeee'}}/>
+        <View style={styles.editToolView}>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            {tools.map((tool, index) => {
+              return (
+                <EditToolView
+                  key={"tool_" + index}
+                  click={() => {
+                    this.toolToBridge(tool.type, index)
+                  }}
+                  icon={this.state.toolSelect[index].select ?
+                    toolSelect[index] : toolDefault[index]
+                  }
+                />
+              )
+            })}
+          </View>
+          {Platform.OS == 'ios' ? this.renderHideEditToolView() : <View />}
+        </View>
+      </View>
+    )
+  }
+
+  render() {
+    const height = PAGE_HEIGHT - navBarPadding - this.props.keyboardPadding - (Platform.OS == 'android' ? 20 : 0)
+    const styleFocused = [
+      Platform.OS == 'android' ? styles.mainContainerFocusedAndroid : styles.mainContainerFocusedIOS,
+      {height: height}
+    ]
+    const styleNormal = [styles.mainContainer, {height: this.state.webViewHeight}]
+    return (
+      <View style={this.props.shouldFocus ? styleFocused : styleNormal}>
+        {this.renderWebView()}
+        {this.props.shouldFocus ? this.renderEditToolView() : <View />}
+      </View>
+    )
+  }
+
+  onBridgeMessage(message) {
+    console.log(message)
+    switch (message) {
+      case GET_FOCUS:
+        this.props.onFocus(true)
+        this.setState({
+          closeTips: {height: 0}
+        })
+        break
+      case LOSE_FOCUS:
+        this.props.onFocus(false)
+        break
+      case LOAD_DRAFT:
+        // this.loadDraft()
+        break
+      default:
+        if (message.indexOf(COUNTER) == 0) {
+          let number = message.substr(message.lastIndexOf('_') + 1, message.length)
+          // this.inputOnChangeWithPayload({wordCount: number})
+        } else if (message.indexOf(CONTENTS) == 0) {
+          let content = message.substr(message.lastIndexOf('_') + 1, message.length)
+          // this.inputOnChangeWithPayload({content: content})
+        } else if (message.indexOf(HEIGHT) == 0) {
+          const height = message.substr(message.lastIndexOf('_') + 1, message.length)
+          this.setState({
+            webViewHeight: MIN_RTE_HEIGHT < parseInt(height) ? parseInt(height) : MIN_RTE_HEIGHT,
+          })
+        }
+        break
+    }
+  }
+
+  toolToBridge = (type, toolIndex) => {
+    this.setState({
+      toolSelect: this.state.toolSelect.map((toolSel, index) => {
+        if (toolIndex == index) {
+          toolSel.select = !toolSel.select
+        }
+        return toolSel
+      })
+    })
+    if (toolIndex == tools.length - 1) {
+      this.webView.sendToBridge("preInsertImg_")
+      // selectPhotoTapped({
+      //   start: this.pickAvatarStart,
+      //   failed: this.pickAvatarFailed,
+      //   cancelled: this.pickAvatarCancelled,
+      //   succeed: this.pickImageSucceed
+      // })
+    } else {
+      this.webView.sendToBridge(type)
+    }
+  }
+
+  pickAvatarStart = () => {
+  }
+
+  pickAvatarFailed = () => {
+  }
+
+  pickAvatarCancelled = () => {
+  }
+
+  pickImageSucceed = (source) => {
+    this.uploadImg(source)
+  }
+
+  uploadImg = (source) => {
+    // let fileUri = ''
+    // if (Platform.OS === 'ios') {
+    //   fileUri = fileUri.concat('file://')
+    // }
+    // fileUri = fileUri.concat(source.uri)
+    //
+    // let fileName = source.uri.split('/').pop()
+    // let uploadPayload = {
+    //   fileUri: fileUri,
+    //   fileName: fileName
+    // }
+    // uploadFile(uploadPayload).then((saved) => {
+    //   let leanImgUrl = saved.savedPos
+    //   var imgDom = leanImgUrl
+    //   this.webView.sendToBridge('editImg_' + imgDom)
+    // }).catch((error) => {
+    //   console.log('upload failed:', error)
+    // })
+  }
+}
+
+class EditToolView extends Component {
+  render() {
+    return (
+      <View style={styles.editToolImgView}>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.click()
+          }}
+        >
+          <Image
+            style={styles.editToolImg}
+            source={this.props.icon}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+const injectedJavaScript = `
+    $(document).ready(function(event) {
+        setTimeout(function(){
+          if (WebViewBridge) {
+            WebViewBridge.onMessage = function (message) {
+              if(message == 'keyboard_hide'){
+                lose_focus();
+              }else if(message.indexOf('editImg_') == 0){
+                var editImg = message.substr(message.indexOf('_') + 1, message.length);
+                set_editImg(editImg);
+              }else if(message.indexOf('editStr_') == 0){
+                var editStr = message.substr(message.indexOf('_') + 1, message.length);
+                set_editStr(editStr);
+              }else if(message.indexOf('preInsertImg_') == 0){
+                lostFocus();
+              }else{
+                set_any(message);
+              }
+            };
+            WebViewBridge.send('LOAD_DRAFT');
+          } else {
+            setTimeout(function(){
+              if (WebViewBridge) {
+                WebViewBridge.onMessage = function (message) {
+                  if(message == 'keyboard_hide'){
+                    set_blur();
+                  }else if(message.indexOf('editImg_') == 0){
+                    var editImg = message.substr(message.indexOf('_') + 1, message.length);
+                    set_editImg(editImg);
+                  }else if(message.indexOf('editStr_') == 0){
+                    var editStr = message.substr(message.indexOf('_') + 1, message.length);
+                    set_editStr(editStr);
+                  }else{
+                    set_any(message);
+                  }
+                };
+                WebViewBridge.send('LOAD_DRAFT');
+              }
+            }, 3000)
+          }
+        }, 300)
+      });
+`;
+
+const mapStateToProps = (state, ownProps) => {
+  return {}
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(RichTextInput)
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flexDirection: 'column',
+    width: PAGE_WIDTH,
+  },
+  mainContainerFocusedIOS: {
+    position: 'absolute',
+    // flex: 0,
+    top: 0,
+    width: PAGE_WIDTH,
+    backgroundColor: '#ffffff'
+  },
+  mainContainerFocusedAndroid: {
+    // position: 'absolute',
+    // flex: 0,
+    // top: 0,
+    width: PAGE_WIDTH,
+    backgroundColor: '#ffffff'
+  },
+  editToolView: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+  },
+  editToolImgView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  editToolImg: {
+    flex: 1,
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  editToolKeyboardHide: {
+    alignItems: "center",
+    justifyContent: 'center',
+    paddingLeft: 25,
+    paddingRight: 25,
+  }
+})
--- app/scenes/scenes.js
+++ app/scenes/scenes.js
@@ -15,11 +15,11 @@ export const scenes = Actions.create(
   <Scene key="root" hideNavBar={true}>
     <Scene key="LAUNCH" component={Launch} />
     <Scene key="HOME" component={Home} />
-    <Scene key="LOGIN" component={Login} initial={true}/>
+    <Scene key="LOGIN" component={Login} />
     <Scene key="REGIST" component={Regist} />
     <Scene key="REGVERIFYCODE" component={RegVerifyCode} />
     <Scene key="RETRIEVE_PASSWORD" component={RetrivevePassword} />
     <Scene key="SETPSW" component={SetPsw}/>
-    <Scene key="FIND_PWD_VERIFY_CODE" component={FindPwdVerifyCode}/>
+    <Scene key="FIND_PWD_VERIFY_CODE" component={FindPwdVerifyCode} initial={true}/>
   </Scene>
 )
