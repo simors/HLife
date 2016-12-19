805295b47f69b26deb2019d220583048afdcae6f
yang24201 <yang24201@126.com>
2016-12-19 11:48:23 +0800

    modify scence

--- app/scenes/scenes.js
+++ app/scenes/scenes.js
@@ -35,14 +35,14 @@ const styles = StyleSheet.create({
 
 export const scenes = Actions.create(
   <Scene key="root" hideNavBar={true}>
-    <Scene key="LAUNCH" component={Launch} />
+    <Scene key="LAUNCH" component={Launch} initial={true}/>
     <Scene key="LOGIN" component={Login} />
     <Scene key="REGIST" component={Regist} />
     <Scene key="RETRIEVE_PWD" component={RetrievePwdVerifyCode}/>
     <Scene key="FIND_PWD_VERIFY_CODE" component={FindPwdVerifyCode}/>
     <Scene key="DCTOR_CERTIFICATION" component={DoctorCertification}/>
     <Scene key="PUBLISH_VIEW_TEST" component={PublishViewTest} />
-    <Scene key="PICKER_TEST" component={PickerTest} initial={true}/>
+    <Scene key="PICKER_TEST" component={PickerTest} />
     <Scene key="COMMON_WEB_VIEW" component={CommonWebView} />
 
     <Scene key="HOME" tabs hideNavBar tabBarStyle={styles.tabBarStyle}>
