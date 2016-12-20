f32f702dea52d2b73384ee076f14196fec1992f1
yang24201 <yang24201@126.com>
2016-12-20 19:44:54 +0800

    add conversation model

--- app/models/messageModels.js
+++ app/models/messageModels.js
@@ -5,6 +5,8 @@ import {Map, List, Record} from 'immutable'
 
 export const MsgClientRecord = Record({
   client: undefined,
+  conversationList: List(),
+  unReadMsgCnt: 0,
 }, 'MsgClientRecord')
 
 export const MessageRecord = Record({
@@ -18,3 +20,68 @@ export const MessageRecord = Record({
   timestamp: undefined,
   attributes: Map(),
 }, 'MessageRecord')
+
+export const ConversationRecord = Record({
+  id: undefined,
+  members: List(),
+  name: undefined,
+  unreadCount: 0,
+  lastMessageAt: undefined,
+  updatedAt: undefined,
+  createdAt: undefined,
+  messages: List(),
+}, 'ConversationRecord')
+
+export class Message extends MessageRecord {
+  static fromLeancloudMessage(lcMsg, payload) {
+    let msg = new Message()
+
+    return msg.withMutations((record)=> {
+      var messageType, text
+      if (lcMsg.content) {
+        messageType = lcMsg.content._lctype
+        text = lcMsg.content._lctext
+      } else {
+        messageType = lcMsg.type
+        text = lcMsg.text
+      }
+      record.set('id', lcMsg.id)
+      record.set('from', lcMsg.from)
+      record.set('type', messageType)
+      record.set('text', text)
+      record.set('conversation', lcMsg.cid)
+      record.set('timestamp', lcMsg.timestamp)
+      record.set('status', 'complete')
+
+      let attrs = lcMsg.content? lcMsg.content._lcattrs: lcMsg.attributes
+      for (let propKey in attrs) {
+        record.setIn(['attributes', propKey], attrs[propKey])
+      }
+      if (messageType === msgTypes.MSG_IMAGE || messageType === msgTypes.MSG_AUDIO) {
+        record.setIn(['attributes', 'uri'], attrs.mediaId)
+        if (payload && payload.uri) {
+          record.setIn(['attributes', 'localUri'], payload.uri)
+        }
+      }
+    })
+  }
+}
+
+export class Conversation extends ConversationRecord {
+  static fromLeancloudConversation(lcConv) {
+    const conv = new Conversation()
+    return conv.withMutations((record)=> {
+      record.set('id', lcConv.id)
+      record.set('name', lcConv.name)
+      record.set('members', List(lcConv.members))
+      record.set('lastMessageAt', lcConv.lastMessageAt)
+      record.set('createdAt', lcConv.createdAt)
+      record.set('updatedAt', lcConv.updatedAt)
+      record.set('unreadCount', lcConv.unreadMessagesCount)
+    })
+  }
+}
+
+export class Messenger extends MessengerRecord {
+
+}
