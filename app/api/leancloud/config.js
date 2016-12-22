016ca473c5ef0186de8df1ef556db271f3387f91
reluo <21404683@qq.com>
2016-12-22 16:40:00 +0800

    afternoon 1222

--- app/api/leancloud/config.js
+++ app/api/leancloud/config.js
@@ -3,24 +3,31 @@
  */
 import AV from 'leancloud-storage'
 import {Map, List, Record} from 'immutable'
-import {BannerItem, AnnouncementItem,ColumnItem, TopicsItem,ShopCategory,ArticleItem} from '../../models/ConfigModels'
+import {
+  BannerItem,
+  AnnouncementItem,
+  ColumnItem,
+  TopicsItem,
+  ShopCategory,
+  ArticleItem
+} from '../../models/ConfigModels'
 import ERROR from '../../constants/errorCode'
 
 export function getBanner(payload) {
   let type = payload.type
   let query = new AV.Query('Banners')
   query.equalTo('type', type)
-  if(typeof payload.geo === 'object') {
+  if (typeof payload.geo === 'object') {
     let point = new AV.GeoPoint(payload.geo);
     query.withinKilometers('geo', point, 10);
   }
-  return query.find().then(function(results){
+  return query.find().then(function (results) {
     let banner = []
     results.forEach((result) => {
       banner.push(BannerItem.fromLeancloudObject(result))
     })
     return new List(banner)
-  }, function(err){
+  }, function (err) {
     err.message = ERROR[err.code] ? ERROR[err.code] : ERROR[9999]
     throw err
   })
@@ -30,17 +37,17 @@ export function getAnnouncement(payload) {
   let type = payload.type
   let query = new AV.Query('Announcement')
   query.equalTo('type', type)
-  if(typeof payload.geo === 'object') {
+  if (typeof payload.geo === 'object') {
     let point = new AV.GeoPoint(payload.geo);
     query.withinKilometers('geo', point, 10);
   }
-  return query.find().then(function(results) {
+  return query.find().then(function (results) {
     let announcement = []
     results.forEach((result) => {
       announcement.push(AnnouncementItem.fromLeancloudObject(result))
     })
     return new List(announcement)
-  }, function(err) {
+  }, function (err) {
     err.message = ERROR[err.code] ? ERROR[err.code] : ERROR[9999]
     throw err
   })
@@ -50,60 +57,61 @@ export function getColumn() {
 
   let query = new AV.Query('ArticleCategory')
 
-   // query.equalTo('type', type)
-    return query.find().then(function(results) {
-      let column = []
-   results.forEach((result) => {
-        column.push(ColumnItem.fromLeancloudObject(result))
-      })
-      return new List(column)
-    }, function(err) {
-      err.message = ERROR[err.code] ? ERROR[err.code] : ERROR[9999]
-      throw err
+  // query.equalTo('type', type)
+  return query.find().then(function (results) {
+    let column = []
+    results.forEach((result) => {
+      column.push(ColumnItem.fromLeancloudObject(result))
     })
+    return new List(column)
+  }, function (err) {
+    err.message = ERROR[err.code] ? ERROR[err.code] : ERROR[9999]
+    throw err
+  })
 }
 
 export function getTopics() {
   let query = new AV.Query('TopicCategory')
-  return query.find().then(function(results) {
+  return query.find().then(function (results) {
     let topics = []
     results.forEach((result) => {
       topics.push(TopicsItem.fromLeancloudObject(result))
     })
     return new List(topics)
-  }, function(err) {
+  }, function (err) {
     err.message = ERROR[err.code] ? ERROR[err.code] : ERROR[9999]
     throw err
   })
 }
 
 
-export function getArticle(){
- let query = new AV.Query('Articles')
- return query.find().then(function(results) {
+export function getArticle() {
+  let query = new AV.Query('Articles')
+  return query.find().then(function (results) {
     let article = []
+    console.log('<<<<><><><><><>', results)
     results.forEach((result) => {
       article.push(ArticleItem.fromLeancloudObject(result))
     })
-     return new List(article)
-  }, function(err) {
+
+    return new List(article)
+  }, function (err) {
     err.message = ERROR[err.code] ? ERROR[err.code] : ERROR[9999]
     throw err
   })
-// }
 }
 
 export function getPickedTopics(payload) {
   let isPicked = payload.isPicked
   let query = new AV.Query('TopicCategory')
   query.equalTo('isPicked', isPicked)
-  return query.find().then(function(results) {
+  return query.find().then(function (results) {
     let topics = []
     results.forEach((result) => {
       topics.push(TopicsItem.fromLeancloudObject(result))
     })
     return new List(topics)
-  }, function(err) {
+  }, function (err) {
     err.message = ERROR[err.code] ? ERROR[err.code] : ERROR[9999]
     throw err
   })
@@ -113,13 +121,13 @@ export function getShopCategories(payload) {
   let query = new AV.Query('ShopCategory')
   query.equalTo('status', 1)
   //query.limit(payload.limit ? payload.limit : 5)
-  return query.find().then(function(results){
+  return query.find().then(function (results) {
     let shopCategories = []
-    results.forEach((result)=>{
+    results.forEach((result)=> {
       shopCategories.push(ShopCategory.fromLeancloudObject(result))
     })
     return new List(shopCategories)
-  }, function(err){
+  }, function (err) {
     err.message = ERROR[err.code] ? ERROR[err.code] : ERROR[9999]
     throw err
   })
