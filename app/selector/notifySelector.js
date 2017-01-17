69ce78cde73b9a95f9b8013bb210712cbcb0d8c0
yang24201 <yang24201@126.com>
2017-01-17 11:13:19 +0800

    add notice tips

--- app/selector/notifySelector.js
+++ app/selector/notifySelector.js
@@ -6,3 +6,14 @@ export function hasNewNotice(state) {
   const unReadCnt = state.NOTICE.get('unReadCount')
   return unReadCnt > 0 ? true : false
 }
+
+export function hasNewNoticeByType(state, type) {
+  let notifyMsg = state.NOTICE.getIn(['notifyMsgByType', type])
+  if (notifyMsg) {
+    let unReadCnt = notifyMsg.get('unReadCount')
+    if (unReadCnt) {
+      return unReadCnt > 0 ? true : false
+    }
+  }
+  return false
+}
