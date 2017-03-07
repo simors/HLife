f009af631c51302efd7bf23c22ec8466a6d5eb70
yang24201 <yang24201@126.com>
2017-03-07 10:19:49 +0800

    add well chosen topic

--- app/components/common/Header.js
+++ app/components/common/Header.js
@@ -150,7 +150,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     top: 0,
-    backgroundColor: '#fafafa',
+    backgroundColor: 'white',
     ...Platform.select({
       ios: {
         paddingTop: normalizeH(20),
