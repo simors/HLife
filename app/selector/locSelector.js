ef430576f5e79e5272113f06524101593df8fbfa
yang24201 <yang24201@126.com>
2017-03-10 14:31:22 +0800

    add loc selector function

--- app/selector/locSelector.js
+++ app/selector/locSelector.js
@@ -2,11 +2,46 @@
  * Created by yangyang on 2017/3/9.
  */
 
+export function getLocation(state) {
+  let location = state.CONFIG.location
+  if (location) {
+    return location.toJS()
+  }
+  return {}
+}
+
+export function getGeopoint(state) {
+  let location = state.CONFIG.location
+  if (location) {
+    let locJs = location.toJS()
+    return {latitude: locJs.latitude, longitude: locJs.longitude}
+  }
+  return {latitude: 0, longitude: 0}
+}
+
 export function getAddress(state) {
   let location = state.CONFIG.location
   if (location) {
     let locJs = location.toJS()
     return locJs.address
+  }
+  return ""
+}
+
+export function getCountry(state) {
+  let location = state.CONFIG.location
+  if (location) {
+    let locJs = location.toJS()
+    return locJs.country
+  }
+  return ""
+}
+
+export function getProvince(state) {
+  let location = state.CONFIG.location
+  if (location) {
+    let locJs = location.toJS()
+    return locJs.province
   }
   return ""
 }
@@ -18,4 +53,31 @@ export function getCity(state) {
     return locJs.city
   }
   return '全国'
+}
+
+export function getDistrict(state) {
+  let location = state.CONFIG.location
+  if (location) {
+    let locJs = location.toJS()
+    return locJs.district
+  }
+  return ""
+}
+
+export function getStreet(state) {
+  let location = state.CONFIG.location
+  if (location) {
+    let locJs = location.toJS()
+    return locJs.street
+  }
+  return ""
+}
+
+export function getStreetNumber(state) {
+  let location = state.CONFIG.location
+  if (location) {
+    let locJs = location.toJS()
+    return locJs.streetNumber
+  }
+  return ""
 }
