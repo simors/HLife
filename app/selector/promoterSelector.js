0a599674986b17a829be148e6c5fd9ca7fc97daf
yang24201 <yang24201@126.com>
2017-03-28 14:02:15 +0800

    add get promoter by id selector function

--- app/selector/promoterSelector.js
+++ app/selector/promoterSelector.js
@@ -6,3 +6,11 @@ export function inviteCode(state) {
   let code = state.PROMOTER.get('inviteCode')
   return code
 }
+
+export function getPromoterById(state, id) {
+  let promoter = state.PROMOTER.getIn(['promoters', id])
+  if (promoter) {
+    return promoter.toJS()
+  }
+  return undefined
+}
