997864eeaf24f999d6ad123ae00096e30ffb69cc
wanpeng <wanpeng19860919@163.com>
2017-04-20 15:06:29 +0800

    <feat> add UShare Sdk

--- ios/HLife/AppDelegate.m
+++ ios/HLife/AppDelegate.m
@@ -14,6 +14,7 @@
 #import "RCTBaiduMapViewManager.h"
 #import "RCTPushNotificationManager.h"
 #import <Pingpp/Pingpp.h>
+#import <UMSocialCore/UMSocialCore.h>
 
 @implementation AppDelegate
 
--- ios/Pods/Target Support Files/Pods-HLife/Pods-HLife-resources.sh
+++ ios/Pods/Target Support Files/Pods-HLife/Pods-HLife-resources.sh
@@ -81,12 +81,16 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "Pingpp/lib/Pingpp.bundle"
   install_resource "$PODS_CONFIGURATION_BUILD_DIR/QBImagePickerController/QBImagePicker.bundle"
   install_resource "RSKImageCropper/RSKImageCropper/RSKImageCropperStrings.bundle"
+  install_resource "UMengUShare/UShareSDK/UMSocialSDK/UMSocialSDKPromptResources.bundle"
+  install_resource "UMengUShare/UShareSDK/UMSocialUI/UMSocialSDKResources.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "Pingpp/lib/Channels/Alipay/AlipaySDK.bundle"
   install_resource "Pingpp/lib/Pingpp.bundle"
   install_resource "$PODS_CONFIGURATION_BUILD_DIR/QBImagePickerController/QBImagePicker.bundle"
   install_resource "RSKImageCropper/RSKImageCropper/RSKImageCropperStrings.bundle"
+  install_resource "UMengUShare/UShareSDK/UMSocialSDK/UMSocialSDKPromptResources.bundle"
+  install_resource "UMengUShare/UShareSDK/UMSocialUI/UMSocialSDKResources.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
