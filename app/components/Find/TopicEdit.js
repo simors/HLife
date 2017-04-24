280e2896402fdbd4f5f68782abb082cee74b85e5
zachary <443428762@qq.com>
2017-04-24 10:17:53 +0800

    我的话题编辑时，选择话题的弹出框改成与发布话题时一致

--- app/components/Find/TopicEdit.js
+++ app/components/Find/TopicEdit.js
@@ -32,6 +32,7 @@ import ArticleEditor from '../common/Input/ArticleEditor'
 import TimerMixin from 'react-timer-mixin'
 import THEME from '../../constants/themes/theme1'
 import uuid from 'react-native-uuid'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const PAGE_WIDTH = Dimensions.get('window').width
 const PAGE_HEIGHT = Dimensions.get('window').height
@@ -284,16 +285,24 @@ class TopicEdit extends Component {
             {this.renderRichText(this.props.topic.content)}
           </View>
 
-          <ModalBox style={styles.modalStyle} entry='top' position="top" ref={"modal3"}>
-            <ScrollView style={{flex: 1, height: PAGE_HEIGHT}}>
-              <Text style={styles.modalShowTopicsStyle}>选择一个主题</Text>
-              <View style={{flexDirection: 'row', flexWrap: 'wrap', alignItems: 'flex-start'}}>
-                {this.renderTopicsSelected()}
-              </View>
-            </ScrollView>
-          </ModalBox>
-
         </View>
+
+
+        <ModalBox style={styles.modalStyle} entry='top' position="top" ref={"modal3"}>
+          <View style={styles.modalTitleContainer}>
+            <Text style={styles.modalTitleTxt}>选择一个主题</Text>
+            <TouchableOpacity onPress={()=>{this.closeModal(this.state.selectedTopic)}} style={{position:'absolute',right:0,top:0}}>
+              <Icon name='ios-close' style={{fontSize:24,height:24}} />
+            </TouchableOpacity>
+          </View>
+          <ScrollView style={{flex: 1, height: PAGE_HEIGHT}}>
+            <View style={{flexDirection: 'row', flexWrap: 'wrap', alignItems: 'flex-start'}}>
+              {this.renderTopicsSelected()}
+            </View>
+          </ScrollView>
+        </ModalBox>
+
+          
       </View>
     );
   }
@@ -408,6 +417,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
     height: PAGE_HEIGHT,
     alignItems: 'flex-start',
+    ...Platform.select({
+      ios: {
+        paddingTop: normalizeH(20),
+      },
+      android: {
+        paddingTop: normalizeH(0)
+      }
+    }),
   },
   modalTextStyle: {
     marginTop: normalizeH(17),
@@ -415,6 +432,20 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     color: "#5a5a5a",
     fontSize: em(12)
+  },
+  modalTitleContainer: {
+    height: 24,
+    width: PAGE_WIDTH - 30,
+    margin:15,
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  modalTitleTxt: {
+    color: "#4a4a4a",
+    fontSize: em(12),
+    lineHeight: 24,
   },
   modalShowTopicsStyle: {
     marginTop: normalizeH(17),
