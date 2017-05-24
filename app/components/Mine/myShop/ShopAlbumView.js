264914c2f4bb84772215e369baabe4e4b6fd6b7f
wanpeng <wanpeng19860919@163.com>
2017-05-24 09:59:29 +0800

    <refactor> 店铺相册展示修改

---
+++ app/components/Mine/myShop/ShopAlbumView.js
@@ -0,0 +1,67 @@
+/**
+ * Created by wanpeng on 2017/5/23.
+ */
+import React, {Component} from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  ScrollView,
+  ListView,
+  TouchableOpacity,
+  Image,
+  Platform,
+  InteractionManager
+} from 'react-native'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {Actions} from 'react-native-router-flux'
+import {em, normalizeW, normalizeH, normalizeBorder} from '../../../util/Responsive'
+import Header from '../../common/Header'
+import ImageGroupViewer from '../../common/Input/ImageGroupViewer'
+
+
+class ShopAlbumView extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return (
+      <View>
+        <Header
+          leftType="icon"
+          leftIconName="ios-arrow-back"
+          leftPress={() => Actions.pop()}
+          title="店铺相册"
+        />
+        <View style={styles.body}>
+          <ScrollView>
+            <View style={{marginTop: normalizeH(10)}}>
+              <ImageGroupViewer images={this.props.album} imageLineCnt={3}/>
+            </View>
+          </ScrollView>
+        </View>
+      </View>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopAlbumView)
+
+const styles = StyleSheet.create({
+  body: {
+    marginTop: normalizeH(64),
+  },
+
+})
