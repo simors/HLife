092fc9575c722c74e14cd511cdf773da38d603b5
yang24201 <yang24201@126.com>
2017-08-18 11:38:23 +0800

    modify UserOrdersViewer

--- app/components/Mine/myOrders/UserOrdersViewer.js
+++ app/components/Mine/myOrders/UserOrdersViewer.js
@@ -7,19 +7,29 @@ import {
   Text,
   StyleSheet,
   InteractionManager,
+  TouchableOpacity,
+  Dimensions,
 } from 'react-native'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import {Actions} from 'react-native-router-flux'
 import Header from '../../common/Header'
+import {em, normalizeW, normalizeH} from '../../../util/Responsive'
+import THEME from '../../../constants/themes/theme1'
 import {fetchUserShopOrders} from '../../../action/shopAction'
 import {activeUserId} from '../../../selector/authSelector'
 import {selectUserOrders} from '../../../selector/shopSelector'
 import {ORDER_STATUS} from '../../../constants/appConfig'
 
+const PAGE_WIDTH = Dimensions.get('window').width
+const PAGE_HEIGHT = Dimensions.get('window').height
+
 class UserOrdersViewer extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      tabType: 0,
+    }
   }
 
   componentWillMount() {
@@ -27,10 +37,104 @@ class UserOrdersViewer extends Component {
       this.props.fetchUserShopOrders({
         more: false,
         buyerId: this.props.currentUserId,
-        orderStatus: [ORDER_STATUS.PAID_FINISHED],
+        orderStatus: undefined,
         limit: 10,
       })
     })
+  }
+
+  toggleTab(type) {
+    this.setState({tabType: type}, ()=> {
+      if (0 == type) {
+
+      } else if (1 == type) {
+
+      } else if (2 == type) {
+
+      }
+    })
+  }
+
+  renderTabBar() {
+    return (
+      <View style={styles.tabBar}>
+        <TouchableOpacity
+          style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+          onPress={()=> {
+            this.toggleTab(0)
+          }}>
+          <View style={[{
+            width: normalizeW(100),
+            height: normalizeH(44),
+            justifyContent: 'flex-end',
+            alignItems: 'center'
+          },
+            this.state.tabType == 0 ?
+            {
+              borderBottomWidth: 3,
+              borderColor: THEME.base.mainColor
+            } : {}]}>
+            <Text style={[{fontSize: em(15), paddingBottom: normalizeH(8)},
+              this.state.tabType == 0 ?
+              {
+                color: THEME.base.mainColor,
+                fontWeight: 'bold',
+              } : {color: '#4A4A4A'}]}
+            >全部</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+          onPress={()=> {
+            this.toggleTab(1)
+          }}>
+          <View style={[{
+            width: normalizeW(100),
+            height: normalizeH(44),
+            justifyContent: 'flex-end',
+            alignItems: 'center'
+          },
+            this.state.tabType == 1 ?
+            {
+              borderBottomWidth: 3,
+              borderColor: THEME.base.mainColor
+            } : {}]}>
+            <Text style={[{fontSize: em(15), paddingBottom: normalizeH(8)},
+              this.state.tabType == 1 ?
+              {
+                color: THEME.base.mainColor,
+                fontWeight: 'bold',
+              } : {color: '#4A4A4A'}]}
+            >待收货</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+          onPress={()=> {
+            this.toggleTab(2)
+          }}>
+          <View style={[{
+            width: normalizeW(100),
+            height: normalizeH(44),
+            justifyContent: 'flex-end',
+            alignItems: 'center'
+          },
+            this.state.tabType == 2 ?
+            {
+              borderBottomWidth: 3,
+              borderColor: THEME.base.mainColor
+            } : {}]}>
+            <Text style={[{fontSize: em(15), paddingBottom: normalizeH(8)},
+              this.state.tabType == 2 ?
+              {
+                color: THEME.base.mainColor,
+                fontWeight: 'bold',
+              } : {color: '#4A4A4A'}]}
+            >已完成</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
   }
 
   render() {
@@ -42,6 +146,9 @@ class UserOrdersViewer extends Component {
           leftPress={() => Actions.pop()}
           title="我的订单"
         />
+        <View style={styles.body}>
+          {this.renderTabBar()}
+        </View>
       </View>
     )
   }
@@ -66,5 +173,18 @@ export default connect(mapStateToProps, mapDispatchToProps)(UserOrdersViewer)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  body: {
+    marginTop: normalizeH(64),
+  },
+  tabBar: {
+    height: normalizeH(44),
+    width: PAGE_WIDTH,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#FFFFFF',
+    borderBottomWidth: 1,
+    borderColor: '#f5f5f5',
   },
 })
