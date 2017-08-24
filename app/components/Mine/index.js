0f3444221f87550e8fde443f3ba5dd6a5b7e2759
yang24201 <yang24201@126.com>
2017-08-24 15:50:38 +0800

    modify Mine page header element size

--- app/components/Mine/index.js
+++ app/components/Mine/index.js
@@ -132,14 +132,14 @@ class Mine extends Component {
       <View style={styles.header}>
         <View style={{flexDirection: 'row', justifyContent: 'flex-end', marginTop: normalizeH(20)}}>
           <TouchableOpacity onPress={()=> Actions.SETTING()} style={styles.setBtnStyle}>
-            <Svg height={45} width={32} icon="set"/>
+            <Svg height={normalizeH(45)} width={normalizeW(32)} icon="set"/>
             <Text style={styles.headerFont}>设置</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.headerMainView}>
           <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
             <TouchableOpacity onPress={() => Actions.PROFILE()}>
-              <Avatar size={60} src={userInfo.avatar} />
+              <Avatar size={normalizeW(60)} src={userInfo.avatar} />
             </TouchableOpacity>
             <View style={{marginLeft: normalizeW(9)}}>
               <Text style={[styles.headerFont, {paddingTop: normalizeH(3), paddingBottom: normalizeH(8)}]}>{userInfo.nickname}</Text>
