5cb10ee8dd3e3af11b22359d31c830e7fd0778ea
yang24201 <yang24201@126.com>
2017-08-28 22:24:44 +0800

    modify edit shop price input

--- app/components/Mine/myShop/EditShopGood.js
+++ app/components/Mine/myShop/EditShopGood.js
@@ -275,6 +275,7 @@ class EditShopGood extends Component {
                     textAlignVertical: 'center',
                     borderColor: '#0f0f0f',
                     width: normalizeW(100),
+                    padding: 0,
                   }}
                 />
                 <Text style={{fontSize: em(17), color: '#5A5A5A', paddingLeft: 8}}>元</Text>
@@ -294,6 +295,7 @@ class EditShopGood extends Component {
                     textAlignVertical: 'center',
                     borderColor: '#0f0f0f',
                     width: normalizeW(100),
+                    padding: 0,
                   }}
                 />
                 <Text style={{fontSize: em(17), color: '#5A5A5A', paddingLeft: 8}}>元</Text>
