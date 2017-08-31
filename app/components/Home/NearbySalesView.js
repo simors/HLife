8ff83ee0158cbfdc147cf74bfa459d417d502e4e
yang24201 <yang24201@126.com>
2017-08-31 15:55:51 +0800

    improve home page nearby sales load

--- app/components/Home/NearbySalesView.js
+++ app/components/Home/NearbySalesView.js
@@ -11,6 +11,7 @@ import {
   TouchableOpacity,
   Platform,
   Image,
+  ListView,
 } from 'react-native'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
@@ -18,9 +19,7 @@ import {Actions} from 'react-native-router-flux'
 import {em, normalizeW, normalizeH, normalizeBorder} from '../../util/Responsive'
 import THEME from '../../constants/themes/theme1'
 import shallowequal from 'shallowequal'
-import {LazyloadView} from '../common/Lazyload'
-import {CachedImage} from "react-native-img-cache"
-import {getThumbUrl} from '../../util/ImageUtil'
+import NearbySalesItem from './NearbySalesItem'
 
 const PAGE_WIDTH = Dimensions.get('window').width
 const PAGE_HEIGHT = Dimensions.get('window').height
@@ -40,87 +39,22 @@ class NearbySalesView extends Component {
     return false
   }
 
-  renderSaleItems() {
+  renderSaleItems(rowData, index) {
+    return <NearbySalesItem promotion={rowData} index={index}/>
+  }
 
-
-    let promotionsView = <View />
-    if(this.props.shopPromotionList && this.props.shopPromotionList.length) {
-      promotionsView = this.props.shopPromotionList.map((item, index)=>{
-        let goodInfo = {
-          id: item.goodId,
-          targetShop: item.shopId,
-          goodsName: item.goodName,
-          price: item.price,
-          originalPrice: item.originalPrice,
-          promotionPrice: item.promotionPrice,
-          coverPhoto: item.coverPhoto,
-          album: item.album,
-          status: item.goodStatus,
-          detail: item.detail,
-          promotion: item.id,
-          updatedAt: item.goodUpdatedAt,
-          promotionType: item.type,
-          promotionAbstract: item.abstract
-        }
-
-        return (
-          <TouchableOpacity key={'promotion_' + index} style={{flex: 1}} onPress={() => {Actions.SHOP_GOODS_DETAIL({goodInfo:goodInfo})}}>
-            <LazyloadView host="homeList" style={styles.saleItemView}>
-              <View style={styles.saleImg}>
-                <CachedImage mutable style={{flex: 1}}
-                       source={{uri: getThumbUrl(item.coverPhoto, normalizeW(100), normalizeW(100))}}/>
-              </View>
-              <View style={styles.saleContent}>
-                <View>
-                  <Text style={styles.itemTitle} numberOfLines={1}>{item.goodName}</Text>
-                </View>
-                <View style={styles.addressTextView}>
-                  <View style={{flexDirection: 'row', width: normalizeW(180)}}>
-                    <Text style={[styles.itemText, {maxWidth: normalizeW(150)}]} numberOfLines={1}>{item.shopName}</Text>
-                  </View>
-                  <View>
-                    <Text style={styles.itemText}>{item.distance + item.distanceUnit}</Text>
-                  </View>
-                </View>
-                <View style={styles.saleAbstract}>
-                  <View style={styles.saleLabel}>
-                    <Text style={styles.saleLabelText}>{item.type}</Text>
-                  </View>
-                  <View style={{marginLeft: normalizeW(10)}}>
-                    <Text style={styles.itemText} numberOfLines={1}>{item.typeDesc}</Text>
-                  </View>
-                </View>
-                <View style={styles.priceView}>
-                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    <Text style={styles.priceText}>¥</Text>
-                    <Text style={[styles.priceText, {marginLeft: normalizeW(5)}]}>{item.promotionPrice}</Text>
-                    {item.originalPrice
-                      ? <Text style={[styles.itemText, {marginLeft: normalizeW(5), textDecorationLine: 'line-through'}]}>
-                          原价 {item.originalPrice}
-                        </Text>
-                      : null
-                    }
-                  </View>
-                  <View>
-                    {item.pv
-                      ? <Text style={styles.itemText}>{item.pv}人看过</Text>
-                      : null
-                    }
-                  </View>
-                </View>
-              </View>
-            </LazyloadView>
-          </TouchableOpacity>
-        )
-      })
+  renderSalesListView() {
+    if(!this.props.shopPromotionList || this.props.shopPromotionList.length == 0) {
+      return <View/>
     }
-
+    let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    let dataSource = ds.cloneWithRows(this.props.shopPromotionList)
     return (
-      <View style={{flex: 1}}>
-        {promotionsView}
-      </View>
+      <ListView
+        dataSource={dataSource}
+        renderRow={(rowData, rowId) => this.renderSaleItems(rowData, rowId)}
+      />
     )
-
   }
 
   render() {
@@ -133,7 +67,7 @@ class NearbySalesView extends Component {
           </View>
         </View>
         <View style={styles.contentItem}>
-          {this.renderSaleItems()}
+          {this.renderSalesListView()}
         </View>
       </View>
     )
@@ -182,72 +116,5 @@ const styles = StyleSheet.create({
     flex: 1,
     marginLeft: normalizeW(18),
     marginRight: normalizeW(18),
-  },
-  saleItemView: {
-    ...Platform.select({
-      ios: {
-        height: normalizeH(130),
-      },
-      android: {
-        height: normalizeH(140),
-      }
-    }),
-    flex: 1,
-    flexDirection: 'row',
-    paddingTop: normalizeH(19),
-    paddingBottom: normalizeH(15),
-    borderBottomWidth: 1,
-    borderColor: '#F5F5F5',
-  },
-  saleImg: {
-    width: normalizeW(100),
-    height: normalizeW(100),
-    paddingLeft: normalizeW(5),
-    paddingRight: normalizeW(5),
-  },
-  saleContent: {
-    flex: 1,
-    marginLeft: normalizeW(15),
-    marginRight: normalizeW(5),
-  },
-  itemTitle: {
-    fontSize: em(17),
-    fontWeight: 'bold',
-    color: '#5A5A5A',
-  },
-  itemText: {
-    fontSize: em(12),
-    color: '#AAAAAA',
-  },
-  addressTextView: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: normalizeH(10),
-  },
-  saleAbstract: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: normalizeH(17),
-  },
-  saleLabel: {
-    backgroundColor: THEME.base.lightColor,
-    borderRadius: 2,
-    padding: 2,
-  },
-  saleLabelText: {
-    color: 'white',
-    fontSize: em(12),
-    fontWeight: 'bold',
-  },
-  priceView: {
-    flexDirection: 'row',
-    marginTop: normalizeH(7),
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  priceText: {
-    fontSize: em(15),
-    fontWeight: 'bold',
-    color: '#00BE96',
   },
 })
